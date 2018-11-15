aaf68078125898b1e914262b4020b7344b55b2c1
Huynh Gia Bao Mai <hmi@edifixio.com>
2018-11-15 10:42:31 +0100

    create read first line

--- src/controllers/uploadController.ts
+++ src/controllers/uploadController.ts
@@ -1,5 +1,8 @@
-import * as csv from 'csv';
+//import * as csv from 'csv';
 import * as fs from 'fs';
+import * as csv from 'fast-csv';
+import * as firstline from 'firstline';
+// var csv = require('fast-csv');
 
 export async function uploadFile(req, res) {
     if (!req['file']) {
@@ -11,14 +14,19 @@ export async function uploadFile(req, res) {
         console.log('file received');
         return res.send({
             success: true,
-            firstRow: await readFile(req['file'].path)
+            firstRow: await readFirstLine(req['file'].path)
         })
     }
 }
 
-export function readFile(fileURL): Promise<any> {
+export function readFirstLine(fileURL): Promise<any> {
     return new Promise<any>(resolve => {
         let firstRow = [];
+        firstline(fileURL).then(data => {
+            firstRow = data.split(";");
+            resolve(firstRow);
+        });
+
         // let parser = csv.parse({ delimiter: ';' }, (err, data) => {
         //     data.every((row, rowNumber) => {
         //         if (rowNumber == 0) {
@@ -30,13 +38,60 @@ export function readFile(fileURL): Promise<any> {
         //         }
         //     });
         // });
-        // fs.createReadStream(fileURL).pipe(parser);
+        // fs.createReadStream(fileURL).pipe(parser);(e)
 
-        fs.createReadStream(fileURL)
-            .pipe(csv.parse({ delimiter: ';' }))
-            .on('data', function (csvrow) {
-                firstRow.push(csvrow[0]);
-                resolve(firstRow);
-            })
+        // fs.createReadStream(fileURL)
+        //     .pipe(csv.parse({ delimiter: ';' }))
+        //     .on('data', function (csvrow) {
+        //         firstRow.push(csvrow[0]);
+        //         resolve(firstRow);
+        //     })
+
+        // var fileStream = fs.createReadStream(fileURL);
+        // var csvStream = csv({ headers: true });
+
+        // fileStream.pipe(csvStream);
+
+        // var rows = [];
+        // var onData = function (row) {
+        //     rows.push(row);
+        //     if (rows.length == 1) {
+        //         row.forEach(col => {
+        //             firstRow.push(col);
+        //         })
+        //         console.log(firstRow);
+        //         resolve(firstRow)
+
+        //         csvStream.emit('donereading'); //custom event for convenience
+        //     }
+        // };
+        // csvStream.on('data', onData);
+        // csvStream.on('donereading', function () {
+        //     fileStream.close();
+        //     csvStream.removeListener('data', onData);
+        //     console.log('got 20 rows', rows);
+        // });
     });
 }
+// var fs = require('fs');
+// var csv = require('fast-csv');
+
+// var fileStream = fs.createReadStream(fileURL);
+// var csvStream = csv({headers: true});
+
+// fileStream.pipe(csvStream);
+
+// var rows = [];
+// var onData = function(row){
+//   rows.push(row);
+//   if (rows.length == 20) {
+//     csvStream.emit('donereading'); //custom event for convenience
+//   }
+// };
+// csvStream.on('data', onData);
+// csvStream.on('donereading', function(){
+//   fileStream.close();
+//   csvStream.removeListener('data', onData);
+//   console.log('got 20 rows', rows);
+// });
+
